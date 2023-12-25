7801d012a074dcc4c2f68265d4f414413c3b208b
afroj07 <afrojkhanafroj70@gmail.com>
2023-12-25 20:55:37 +0530

    localStorage define

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
+let TodoList= localStorage.getItem('List');
 
-let todoList = [
+let todoList = TodoList?JSON.parse(TodoList): [
     {
        
     }
 ];
 
 
-
+displayItem();
 function addToDo(){
     let InputElement = document.querySelector('#todo-input');
     let dateInput = document.querySelector("#todo-date");
      let todoItem = InputElement.value;
      let dueDate =  dateInput.value;
      todoList.push({item:todoItem, date:dueDate});
+     localStorage.setItem('List', JSON.stringify(todoList));
      InputElement.value='';
      dateInput.value='';
      displayItem();
@@ -24,7 +26,7 @@ function displayItem(){
         let {item,date} = todoList[i];
         itemBox +=  `<span>${item}</span> 
         <span>${date}</span>
-        <button class='btn-delete' onclick="todoList.splice(${i}, 1);
+        <button class='btn-delete' onclick="todoList.splice(${i}, 1); localStorage.removeItem('List');
         displayItem();">Delete</button>
         <hr>
       `;
